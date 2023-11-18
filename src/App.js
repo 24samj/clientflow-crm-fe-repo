3ac3dc08e51a7261704157d569262e34a5cf7377
Sumit Chakraborty <csumit961@gmail.com>
2023-11-18 14:01:48 +0530

    final-build

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import "@coreui/coreui/dist/css/coreui.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import { Route, Routes } from "react-router-dom";
+import Auth from "./Pages/Auth.jsx";
+import Admin from "./Pages/Admin.jsx";
+import Customer from "./Pages/Customer.jsx";
+import Engineer from "./Pages/Engineer.jsx";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Routes>
+                <Route path="/engineer" element={<Engineer />}></Route>
+                <Route path="/admin" element={<Admin />}></Route>
+                <Route path="/customer" element={<Customer />}></Route>
+                <Route path="/" element={<Auth />}></Route>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
